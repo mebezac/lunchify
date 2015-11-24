dabe59b0d26e138ffc732150cb2c3bccbefb1cd8
Zac Clay <zacclay@gmail.com>
2015-11-23 19:42:33 -0500

    Heroku ready

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,15 @@ use Mix.Config
 # which you typically run after static files are built.
 config :lunchify, Lunchify.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "zac-lunchify.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+config :lunchify, Lunchify.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -55,7 +62,3 @@ config :logger, level: :info
 #
 #     config :lunchify, Lunchify.Endpoint, server: true
 #
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
